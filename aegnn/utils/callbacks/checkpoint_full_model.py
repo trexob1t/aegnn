a545d55cdcc4159c8178e8c663e5fece1ebbf329
trexob1t <adrian.bytyqi@student.uni-tuebingen.de>
2024-08-17 09:58:57 +0200

    should fix checkpoint saving with multi-gpu (directory not found error)

--- aegnn/utils/callbacks/checkpoint_full_model.py
+++ aegnn/utils/callbacks/checkpoint_full_model.py
@@ -9,7 +9,10 @@ class FullModelCheckpoint(pl.callbacks.ModelCheckpoint):
 
     def _save_model(self, trainer: pl.Trainer, filepath: str) -> None:
         trainer.dev_debugger.track_checkpointing_history(filepath)
-        if trainer.should_rank_save_checkpoint:
+        if trainer.should_rank_save_checkpoint and trainer.global_rank == 0:
             self._fs.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+        trainer.accelerator.barrier() # Ensure all gpus wait until directory is made and then save model
+
         torch.save(trainer.model.state_dict(), filepath)
         logging.debug(f"Save model checkpoint @ {filepath}")
